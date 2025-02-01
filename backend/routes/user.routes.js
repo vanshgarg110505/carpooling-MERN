5061b0085dd00d0e32fd593d6394fbb106857462
Vanshika <vanshikaduggal13@gmail.com>
2025-02-01 14:04:00 +0530

    user registration done

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const {body}=require("express-validator")
 const userController = require('../controllers/user.controller')
 
-
+{/*HER WE HAVE CREATED THE VALIDATIONS ONLY BUT IF THERE WOULD BE ANY MISTAKE OR ERROR THEN IT WOULD BE HANDLED IN CONTROLLERS (REISTERUSER) */}
 router.post('/register',[body('email').isEmail().withMessage('Invalid Email'),
     body('fullname.firstname').isLength({min:3}).withMessage('First namemust be atleat 3 character long'),
     body('password').isLength({min:6}).withMessage('Password must be atleat 6 character long'),
