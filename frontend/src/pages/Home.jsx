07934a248d9bd2fc1cce080831d36ef4606ee29d
Vanshika <vanshikaduggal13@gmail.com>
2025-02-06 23:44:59 +0530

    home-update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-     <img className='w-16 absolute left-5 top-5' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" />
+      <img
+        className="w-16 absolute left-5 top-5"
+        src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
+      />
+      <div className="h-screen w-screen">
+        {/*IMAGE FOR TEMPORARY USE */}
+        <img className="h-full w-full object-cover" src="https://miro.medium.com/v2/resize:fit:1400/0*gwMx05pqII5hbfmX.gif" />
+      </div>
+      <div className="bg-white absolute">
+        <h4>Find a trip</h4>
+        <form>
+          <input type="text" placeholder="Add a pick-up location"/>
+          <input type="text" placeholder="Enter your destination"/>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
